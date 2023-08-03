175c6de2592899a5fd9daedb9a0bf4aaafc89bcd
Ankit <ankitxtowod@gmail.com>
2023-08-03 15:03:06 +0530

    update

--- app.py
+++ app.py
@@ -23,23 +23,22 @@ with open('model.pkl', 'rb') as model_file:
 # tfidf = joblib.load('vectorizer.pkl')
 # model = joblib.load('model.pkl')
 def transform_text(text):
-    text = text.lower() # lowercase
-    text = nltk.word_tokenize(text)  #Tokenization 
+    text = text.lower()  # lowercase
+    text = nltk.word_tokenize(text)  # Tokenization
     y = []
     for i in text:
         if i.isalnum():
-            y.append(i) 
+            y.append(i)
 
-    text = y[:] 
+    text = y[:]
     y.clear()
     for i in text:
         if i not in stopwords.words('english') and i not in string.punctuation:
-
             y.append(i)
-    text = y[:] 
+    text = y[:]
     y.clear()
-    
-    for i in text: 
+
+    for i in text:
         y.append(ps.stem(i))
 
     return " ".join(y)
