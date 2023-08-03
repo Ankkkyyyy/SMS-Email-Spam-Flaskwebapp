6c90d9ef3f12a3ae43a9f041a1ebd2b5e2b1d8bc
Ankit <ankitxtowod@gmail.com>
2023-08-03 18:10:24 +0530

    update

--- app.py
+++ app.py
@@ -1,15 +1,11 @@
 from flask import Flask, jsonify ,render_template,url_for
-import pickle
+# import pickle
 import joblib as joblib
-import nltk
+import sklearn
+from flask import request as req
+from textTransformer import transform_text
 
-from nltk.corpus import stopwords
-from nltk.stem.porter import PorterStemmer
-import  sklearn
-import string
-from flask import request as req 
 app = Flask(__name__)
-ps = PorterStemmer() 
 
 
 # tfidf = pickle.load('vectorizer.pkl','rb')
@@ -22,26 +18,7 @@ ps = PorterStemmer()
 
 tfidf = joblib.load('vectorizer2.pkl')
 model = joblib.load('model2.pkl')
-def transform_text(text):
-    text = text.lower()  # lowercase
-    text = nltk.word_tokenize(text)  # Tokenization
-    y = []
-    for i in text:
-        if i.isalnum():
-            y.append(i)
 
-    text = y[:]
-    y.clear()
-    for i in text:
-        if i not in stopwords.words('english') and i not in string.punctuation:
-            y.append(i)
-    text = y[:]
-    y.clear()
-
-    for i in text:
-        y.append(ps.stem(i))
-
-    return " ".join(y)
 
 @app.route('/')
 def index():
@@ -55,7 +32,7 @@ def detector():
 
        transformed_msg = transform_text(data)
        vector_input=tfidf.transform([transformed_msg])
-       result =model.predict(vector_input)[0]
+       result =model.predict(vector_input)
        if result ==1:
            output = "Spam"
        else:
@@ -75,6 +52,7 @@ def spam_detector_api():
     transformed_msg = transform_text(data)
     vector_input = tfidf.transform([transformed_msg])
     result = model.predict(vector_input)[0]
+
     
     if result == 1:
         output = "Spam"
