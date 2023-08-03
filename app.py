4fdb73ccd5bea1922442fac4857dc147708a7ba3
Ankit <ankitxtowod@gmail.com>
2023-08-03 15:33:58 +0530

    update

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, jsonify ,render_template,url_for
-import pickle
-# import joblib as joblib
+# import pickle
+import joblib as joblib
 import nltk
 
 from nltk.corpus import stopwords
@@ -14,14 +14,14 @@ ps = PorterStemmer()
 
 # tfidf = pickle.load('vectorizer.pkl','rb')
 # model = pickle.load('model.pkl','rb')
-with open('vectorizer.pkl', 'rb') as vectorizer_file:
-    tfidf = pickle.load(vectorizer_file)
+# with open('vectorizer.pkl', 'rb') as vectorizer_file:
+#     tfidf = pickle.load(vectorizer_file)
+#
+# with open('model.pkl', 'rb') as model_file:
+#     model = pickle.load(model_file)
 
-with open('model.pkl', 'rb') as model_file:
-    model = pickle.load(model_file)
-
-# tfidf = joblib.load('vectorizer.pkl')
-# model = joblib.load('model.pkl')
+tfidf = joblib.load('vectorizer.pkl')
+model = joblib.load('model.pkl')
 def transform_text(text):
     text = text.lower()  # lowercase
     text = nltk.word_tokenize(text)  # Tokenization
